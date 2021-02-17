722546343d57bde2ea93307faf3a407b04ca4f07
Jan Hesters <jan.hesters@rwth-aachen.de>
2021-02-17 14:11:34 +0100

    feat: add SEO
    Adds SEO with Next SEO.
    
    closes #10

--- src/features/landing/landing-page-component.tsx
+++ src/features/landing/landing-page-component.tsx
@@ -1,4 +1,6 @@
 import type { TFunction } from 'next-i18next';
+import { NextSeo } from 'next-seo';
+import { Fragment } from 'react';
 
 import { Anchor, Heading, Main } from './landing-page-component-styles';
 
@@ -7,19 +9,23 @@ type LandingPageProps = {
 };
 
 const LandingPageComponent = ({ t }: LandingPageProps) => (
-  <Main>
-    <Heading>
-      {t('landing:welcome-to') + ' '}
-      <Anchor
-        href="https://janhesters.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {t('common:appName')}
-      </Anchor>
-      !
-    </Heading>
-  </Main>
+  <Fragment>
+    <NextSeo title={t('common:appName')} />
+
+    <Main>
+      <Heading>
+        {t('landing:welcome-to') + ' '}
+        <Anchor
+          href="https://janhesters.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t('common:appName')}
+        </Anchor>
+        !
+      </Heading>
+    </Main>
+  </Fragment>
 );
 
 export default LandingPageComponent;
